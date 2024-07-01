abe680cd703aeb47d211c7cdb6fcd8f14fe96230
Joemar Palting <joemarpalting19@gmail.com>
2024-07-01 15:08:16 +0800

    Added backend base url

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://gut-to-know-game-backend.onrender.com',
         secure: false,
         changeOrigin: true,
       }
