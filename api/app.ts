d7f19b6d977538fb3344e2d1a05acbaeb9c31cdc
Joemar Palting <joemarpalting19@gmail.com>
2024-07-01 19:39:15 +0800

    removed method

--- api/app.ts
+++ api/app.ts
@@ -39,7 +39,6 @@ app.use(cookieParser());
 const corsOptions = {
   origin: process.env.FRONTEND_URL || "http://localhost:8080",
   credentials: true,
-  methods: ["GET", "POST"],
 };
 
 // Enable CORS
