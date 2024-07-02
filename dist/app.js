d99a1a593a7c235bdd8b194973055503cbd97d2a
Joemar Palting <joemarpalting19@gmail.com>
2024-07-02 14:55:59 +0800

    backend update

--- dist/app.js
+++ dist/app.js
@@ -27,15 +27,28 @@ mongoose_1.default
 });
 // Initialize Express
 const app = (0, express_1.default)();
-// Parse URL-encoded bodies
-app.use((0, cookie_parser_1.default)());
 // Enable CORS
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3000",
+    origin: ["https://gut-to-know.vercel.app", "http://localhost:8080"],
     credentials: true,
 }));
+// Parse URL-encoded bodies
+app.use((0, cookie_parser_1.default)());
 // Parse JSON bodies
 app.use(express_1.default.json());
+// Middleware to log all requests
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+// Middleware to log CORS headers
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://gut-to-know.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    console.log("CORS headers set for https://gut-to-know.vercel.app");
+    next();
+});
 // Define a route
 app.use("/api/auth", index_js_1.authRouter);
 app.use("/api/question", index_js_1.questionRouter);
