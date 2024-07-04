80fe7ca434be8f8420b06ba5c03a5f7b2de44696
Joemar Palting <joemarpalting19@gmail.com>
2024-07-04 23:28:28 +0800

    fixed backend

--- api/app.ts
+++ api/app.ts
@@ -55,10 +55,7 @@ app.use((req, res, next) => {
 
 // Middleware to log CORS headers
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", [
-    "https://gut-to-know.vercel.app",
-    "http://localhost:8080",
-  ]);
+  res.header("Access-Control-Allow-Origin", "https://gut-to-know.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
